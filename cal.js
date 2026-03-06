02d74ed659f53b1b6ed05ce184aabd34061bbd24
SathishRSK <sathishkumar1456@gmail.com>
2026-03-06 13:13:19 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,37 @@
+const calcy = () =>{
+  let english = document.getElementById('english').value;
+  let maths = document.getElementById('maths').value;
+  let comp = document.getElementById('comp').value;
+  let phy = document.getElementById('phy').value;
+  let Tamil = document.getElementById('Tamil').value;
+  let grades = "";
+
+
+  let totalGrades = (parseFloat(english) + parseFloat(Tamil) + parseFloat(maths) + parseFloat(comp) + parseFloat(phy)).toFixed(2);
+
+  let perc =(((totalGrades/500) * 100));
+  
+
+  debugger;
+
+  if(perc <= 100  && perc >= 80){
+    grades = 'A';
+  }else if(perc <= 79  && perc >= 60){
+     grades = 'B';
+  }else if(perc <= 59  && perc >= 40){
+     grades = 'C';
+  }else{
+     grades = 'F';
+  }
+
+  if(perc >= 39.5){
+    document.getElementById('showData').innerHTML = ` Out of 400 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}.  `
+    document.getElementById('result').innerHTML = ` PASS `
+  }
+  else{
+    document.getElementById('showData').innerHTML = ` Out of 400 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}. `
+    document.getElementById('result').innerHTML = ` FAIL `
+  }
+
+  
+}
